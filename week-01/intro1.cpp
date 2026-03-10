7e9a4dd36005f4276b9c69b99ddc3db54a1a2294
João Pedro Ribeiro <joaopedrorib@hotmail.com>
2026-03-10 16:28:07 -0300

    add(intro1)

--- week-01/intro1.cpp
+++ week-01/intro1.cpp
@@ -12,26 +12,17 @@ int main(){
     }
 
     int complement[(int)1e4+10] = {0};
-    int compl;
+    int comp;
     int count = 0;
     for(int i=0; i < n; i++){
-        compl = s - v[i];
-        if(compl > 0){
-            complement[compl]++;
-            complement[v[i]]
+        count += complement[v[i]];
+        comp = s - v[i];
+        if(comp > 0){
+            complement[comp]++;
         }
     }
 
-    for(int i=0; i < n; i++){
-
-    }
+    cout << count;
 
     return 0;
 }
-
-3 5
-
-2 3 7
-
-[3]=1
-[2]=1
