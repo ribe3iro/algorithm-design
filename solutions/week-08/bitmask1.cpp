c590c9a9f31350338c231b1eec62573a951c8bbf
João Pedro Ribeiro da Silva <joaopedrorib@hotmail.com>
2026-05-06 17:53:29 -0300

    wip(bitmask1)

--- solutions/week-08/bitmask1.cpp
+++ solutions/week-08/bitmask1.cpp
@@ -6,46 +6,49 @@ const int MAX_NODES = 20;
 
 int n, m;
 int graph[MAX_NODES][MAX_NODES];
-int memo[(1 << MAX_NODES)-1];
 int visited = 0;
 
 int solve(int node){
     visited = visited | (1 << node);
 
-    if(visited == (1 << n)-1){
+    if(visited == ((1 << n)-1)){
         return 0;
     }
 
     int min_dist = INT_MAX;
-    int curr_dist = 0;
-    for(int i = 0; i < MAX_NODES; i++){
-        curr_dist = graph[node][j]
-        if(curr_dist > 0 && (visited & (1 << node))){
-            min_dist = min(min_dist, curr_dist + solve(j));
+    int neighbor_dist, returned_dist;
+    for(int i = 1; i < n; i++){
+        neighbor_dist = graph[node][i];
+        if((neighbor_dist > 0) && !(visited & (1 << i))){
+            returned_dist = solve(i);
+            if(returned_dist < INT_MAX){
+                min_dist = min(min_dist, neighbor_dist + returned_dist);
+            }
         }
     }
-    if(min_dist < INT_MAX){
-        memo[node] = min_dist;
-    }
 
-    return memo[node];
+    visited = visited & (~(1 << node));
+
+    return min_dist;
 }
 
 int main(){
     cin >> n >> m;
 
     fill(&graph[0][0], &graph[0][0] + (MAX_NODES * MAX_NODES), 0);
-    fill(&memo[0], &memo[0] + (MAX_NODES), -1);
     int u, v, d;
-    for(int i = 0; i < MAX_NODES; i++)
-        for(int j = 0; j < MAX_NODES; j++){
-            cin >> u, v, d;
-            u--; v--;
-            graph[u][v] = d;
-            graph[v][u] = d;
-        }
+    for(int i = 0; i < m; i++){
+        cin >> u >> v >> d;
+        u--; v--;
+        graph[u][v] = d;
+        graph[v][u] = d;
+    }
 
-    cout << solve(0) << endl;
+    int min_dist = solve(0);
+    if(min_dist == INT_MAX){
+        min_dist = -1;
+    }
+    cout << min_dist << endl;
 
     return 0;
 }
