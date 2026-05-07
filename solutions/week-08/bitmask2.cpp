d361ac8b994bcee219a940b52df7eee134147529
João Pedro Ribeiro da Silva <joaopedrorib@hotmail.com>
2026-05-07 20:32:01 -0300

    add(bitmask2)

--- solutions/week-08/bitmask2.cpp
+++ solutions/week-08/bitmask2.cpp
@@ -3,20 +3,36 @@
 using namespace std;
 
 const int MAX_GROUPS = 100;
+const int MAX_DISTRICTS = 20;
 
 int n,k;
 int groups[MAX_GROUPS];
 int coverage = 0;
 
 int solve(int group_index){
-    int group = groups[group_index];
-    coverage = coverage | group;
-
     if(coverage == (1 << n)-1){
         return 0;
     }
 
-    
+    if(group_index >= k){
+        return INT_MAX;
+    }
+
+    // not including current group
+    int count_not_including = solve(group_index+1);
+
+    // including current group
+    int group = groups[group_index];
+    int old_coverage = coverage;
+    coverage = coverage | group;
+    int count_including = solve(group_index+1);
+    if(count_including < INT_MAX){
+        count_including += 1;
+    }
+    int min_antennas = min(count_not_including, count_including);
+    coverage = old_coverage;
+
+    return min_antennas;
 }
 
 int main(){
@@ -34,9 +50,9 @@ int main(){
         }
     }
 
-    int min_antennas = -1;
-    for(int i = 0; i < k; i++){
-        min_antennas = min(min_antennas, solve(i));
+    int min_antennas = solve(0);
+    if(min_antennas == INT_MAX){
+        min_antennas = -1;
     }
 
     cout << min_antennas << endl;
