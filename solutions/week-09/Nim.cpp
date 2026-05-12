ca6cd3013963e7d2935fb7697e95623bc24921ac
João Pedro Ribeiro da Silva <joaopedrorib@hotmail.com>
2026-05-12 18:49:50 -0300

    wip(Nim)

---
+++ solutions/week-09/Nim.cpp
@@ -0,0 +1,22 @@
+class Nim{
+    public:
+        int heaps[];
+
+        Nim(int heaps[]){
+            this->heaps = heaps;
+        }
+
+        int getGrundyNumber(){
+            int num_heaps = heaps.size();
+            int 
+
+            for(int i = 0; i < num_heaps; i++){
+
+            }
+        }
+    
+    private:
+        solveGrundyNumber(){
+
+        }
+};
